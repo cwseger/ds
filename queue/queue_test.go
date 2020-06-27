51d675a058623ed55a9fa42bb9031b23cd94b98e
Chance Seger <cwseger@gmail.com>
2020-06-26 21:20:37 -0400

    wrapped up tests for queue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -30,3 +30,32 @@ func TestEnqueueAndPeek(t *testing.T) {
 		t.Errorf("Peek() did not return value of the element at the head. got: %v, want: %v", peekOut, 10)
 	}
 }
+
+func TestDequeue(t *testing.T) {
+	q := queue.New()
+	q.Enqueue(10)
+	q.Enqueue(20)
+	dequeueOut := q.Dequeue()
+	if dequeueOut != 10 {
+		t.Errorf("Dequeue() did not return first element in the queue. got: %v, want: %v", dequeueOut, 10)
+	}
+	dequeueOut = q.Dequeue()
+	if dequeueOut != 20 {
+		t.Errorf("Dequeue() did not return first element in the queue. got: %v, want: %v", dequeueOut, 20)
+	}
+	dequeueOut = q.Dequeue()
+	if dequeueOut != nil {
+		t.Errorf("Dequeue() did not return nil when no elements in the queue. got: %v, want: %v", dequeueOut, nil)
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	q := queue.New()
+	if !q.IsEmpty() {
+		t.Errorf("IsEmpty() did not return true when no elements in queue. got: %v, want: %v", q.IsEmpty(), true)
+	}
+	q.Enqueue(10)
+	if q.IsEmpty() {
+		t.Errorf("IsEmpty() did not return false when elements are in the queue. got: %v, want: %v", q.IsEmpty(), false)
+	}
+}
